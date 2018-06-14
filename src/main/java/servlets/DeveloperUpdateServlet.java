c96b0aca176f8c1cf937c1fed71f257e433cc9e0
Oleksii Movchan <liohamitec@liohamitec.liohadomain>
2018-06-14 11:21:57 +0300

    heroku tryout2

--- src/main/java/servlets/DeveloperUpdateServlet.java
+++ src/main/java/servlets/DeveloperUpdateServlet.java
@@ -35,5 +35,4 @@ public class DeveloperUpdateServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
     }
-    
 }
