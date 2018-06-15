0cdef41a3a7357294d027862990d37c7df06cb3d
Oleksii Movchan <liohamitec@liohamitec.liohadomain>
2018-06-15 10:03:31 +0300

    heroku tryout 10:03

--- src/main/java/util/SessionFactoryCreator.java
+++ src/main/java/util/SessionFactoryCreator.java
@@ -7,9 +7,13 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SessionFactoryCreator {
 
     public static SessionFactory createFactory() {
+        /*
         Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(Developer.class);
         cfg.addAnnotatedClass(Skill.class);
@@ -20,5 +24,23 @@ public class SessionFactoryCreator {
         builder.applySettings(cfg.getProperties());
         ServiceRegistry serviceRegistry = builder.build();
         return cfg.buildSessionFactory(serviceRegistry);
+        */
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(Developer.class);
+        cfg.addAnnotatedClass(Skill.class);
+        cfg.configure("hibernate.cfg.xml");
+
+        Map<String,String> jdbcUrlSettings = new HashMap<>();
+        String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (null != jdbcDbUrl) {
+            jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+        }
+
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+        builder.applySettings(cfg.getProperties());
+        builder.applySettings(jdbcUrlSettings);
+        ServiceRegistry serviceRegistry = builder.build();
+        return cfg.buildSessionFactory(serviceRegistry);
+
     }
 }
